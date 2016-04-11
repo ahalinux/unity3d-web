8118456ba2c37d63600daa10cef33c8358de3a18
ahalinux <575713113@qq.com>
2016-04-11 21:56:40 +0800

    unity3d get and post

--- app.js
+++ app.js
@@ -1,49 +1,47 @@
 var http = require('http');
 var url = require('url');
-var querystring = require('querystring');
+var httpParam = require('./http_parm');
 
 http.createServer(function(req,res){
     var pathname = url.parse(req.url).pathname;
-    var parmstr = url.parse(req.url).query;
-    var parm = querystring.parse(parmstr);
-
-    console.log(pathname);
-    console.log(parmstr);
-    console.log(parm);
-
+    httpParam.init(req, res);
     switch(pathname){
-        case '/index' : resIndex(res);
+
+        case '/get' : resGet(res,req);
             break;
-        case '/img'   : resImage(res);
+
+        case '/post' : resPost(res, req);
             break;
-        default       : resDefault(req,res);
+
+        default : resDefault(res);
             break;
+
     }
-
-    //console.log(req.method);
-    //console.log(req.headers);
-
 }).listen(1337, "192.168.1.14");
 
-function resIndex(res){
-    /* 获取当前index.html的路径 */
-    var readPath = __dirname + '/' +url.parse('index.html').pathname;
-    var indexPage = fs.readFileSync(readPath);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(indexPage);
+function resDefault(res){
+    res.end("Default Process");
 }
 
-function resImage(res){
-    /* 获取当前image的路径 */
-    var readPath = __dirname + '/' +url.parse('logo.png').pathname;
-    var indexPage = fs.readFileSync(readPath);
-    res.writeHead(200, { 'Content-Type': 'image/png' });
-    res.end(indexPage);
+/** 同步GET
+function resGet(res){
+    var getData = httpParam.GET();
+    res.end(getData);
+}
+*/
+
+function resGet(res, req){
+    req.setEncoding('utf8');
+    httpParam.GET(function(getData){
+        res.end(getData);
+    });
 }
 
-function resDefault(req,res){
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end((querystring.parse(url.parse(req.url).query)).username);
+function resPost(res, req){
+    req.setEncoding('utf8');
+    httpParam.POST(function(postData){
+        res.end(postData);
+    });
 }
 
 console.log('Server running at http://192.168.1.14:1337');
